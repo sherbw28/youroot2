035509b6cb54cac577dbbda5a0727752fdce1642
Shimizu-04 <rin0417shimizu@gmail.com>
2022-02-24 19:19:32 +0900

    detail.html 評価機能の見た目

--- static/js/card.js
+++ static/js/card.js
@@ -18,3 +18,16 @@ $(function() {
         $('.l-wrapper_01').toggleClass('is-surface').toggleClass('is-reverse');
     });
 });*/
+$(function() {
+    $('.card_switch_03').on('click', function() {
+
+        $($(this).parents('.l-wrapper_01')).toggleClass('is-surface_03').toggleClass('is-reverse_03');
+    });
+});
+
+$(function() {
+    $('.card_switch_03_back').on('click', function() {
+
+        $($(this).parents('.l-wrapper_01')).toggleClass('is-surface_03').toggleClass('is-reverse_03');
+    });
+});
