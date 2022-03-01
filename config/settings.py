0c26482c1960dc83e395b4ad2a3ed97495c719cd
sherbw28 <sherbw28@outlook.jp>
2022-03-01 21:47:03 +0900

    allowed_host変更

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-72el(^+66^crh0wbs5#*#@o3cft^=2hs-29zmuya(i958i5h^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
